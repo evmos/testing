83629f52f1638e9ea3838604b4d745330916c463
Freddy Caceres <facs95@gmail.com>
2022-12-18 18:49:02 -0500

    fix node script

--- evmos-devnet/start.sh
+++ evmos-devnet/start.sh
@@ -6,6 +6,10 @@ MONIKER="mymoniker"
 DATA_DIR=$(mktemp -d -t evmos-datadir.XXXXX)
 MNEMONIC="stumble tilt business detect father ticket major inner awake jeans name vibrant tribe pause crunch sad wine muscle hidden pumpkin inject segment rocket silver"
 
+GENESIS=$DATA_DIR/config/genesis.json
+TEMP_GENESIS=$DATA_DIR/config/tmp_genesis.json
+CONFIG=$DATA_DIR/config/config.toml
+
 echo "create and add new keys"
 echo $MNEMONIC | ./evmosd keys add $KEY --home $DATA_DIR --no-backup --chain-id $CHAINID --keyring-backend test --recover
 echo "init Evmos with moniker=$MONIKER and chain-id=$CHAINID"
@@ -14,6 +18,10 @@ echo "prepare genesis: Allocate genesis accounts"
 ./evmosd add-genesis-account \
 "$(./evmosd keys show $KEY -a --home $DATA_DIR --keyring-backend test)" 100000000000000000000000000000000aevmos,1000000000000000000stake \
 --home $DATA_DIR --keyring-backend test
+
+# Set gas limit in genesis
+cat $GENESIS | jq '.consensus_params["block"]["max_gas"]="10000000"' > $TEMP_GENESIS && mv $TEMP_GENESIS $GENESIS
+
 echo "prepare genesis: Sign genesis transaction"
 ./evmosd gentx $KEY 1000000000000000000stake --keyring-backend test --home $DATA_DIR --keyring-backend test --chain-id $CHAINID
 echo "prepare genesis: Collect genesis tx"
@@ -25,6 +33,9 @@ echo "prepare genesis: Run validate-genesis to ensure everything worked and that
 
 sed -i 's/prometheus = false/prometheus = true/g' $DATA_DIR/config/config.toml
 sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "0.0.0.0:6060"/g' $DATA_DIR/config/config.toml
+# Change to 1s to have the same default configuration as v9
+sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' "$CONFIG"
+
 
 echo "running evmos with extra flags $EXTRA_FLAGS"
 
@@ -33,4 +44,4 @@ echo "starting evmos node $i in background ..."
 --keyring-backend test --home $DATA_DIR \
 >$DATA_DIR/node.log $EXTRA_FLAGS
 
-# curl localhost:8545 -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBalance","params":["0x1cF80B60F4F58221AaFFDBb2e513C0Ef1F809494", "latest"],"id":1,"jsonrpc":"2.0"}'
+# curl localhost:8545 -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBalance","params":["0x1cF80B60F4F58221AaFFDBb2e513C0Ef1F809494", "latest"],"id":1,"jsonrpc":"2.0"}
