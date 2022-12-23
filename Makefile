74867f21064aa33230a6454a7c9541a9c1015079
Tom <tomasguerraalda@hotmail.com>
2022-12-23 15:49:03 -0300

    chore: add multiple node config

---
+++ Makefile
@@ -0,0 +1,44 @@
+###############################################################################
+###                                Localnet                                 ###
+###############################################################################
+
+LOCALNET_SETUP_FILE=localnet/docker-compose.yml
+
+# Build image for a local testnet
+localnet-build:
+	@$(MAKE) -C localnet
+
+# Start a 4-node testnet locally
+localnet-start: localnet-stop localnet-build
+	@if ! [ -f localnet/build/node0/$(EVMOS_BINARY)/config/genesis.json ]; then docker run --rm -v $(CURDIR)/localnet/build:/evmos:Z localnet/node "./evmosd testnet init-files --v 4 -o /evmos --keyring-backend=test --starting-ip-address 192.167.10.2"; fi
+	docker-compose -f $(LOCALNET_SETUP_FILE) up -d
+
+# Stop testnet
+localnet-stop:
+	docker-compose -f $(LOCALNET_SETUP_FILE) down
+
+# Clean testnet
+localnet-clean:
+	docker-compose -f $(LOCALNET_SETUP_FILE) down
+	sudo rm -rf build/*
+
+ # Reset testnet
+localnet-unsafe-reset:
+	docker-compose -f $(LOCALNET_SETUP_FILE) down
+ifeq ($(OS),Windows_NT)
+	@docker run --rm -v $(CURDIR)\build\node0\evmosd:/evmos\Z localnet/node "./evmosd tendermint unsafe-reset-all --home=/evmos"
+	@docker run --rm -v $(CURDIR)\build\node1\evmosd:/evmos\Z localnet/node "./evmosd tendermint unsafe-reset-all --home=/evmos"
+	@docker run --rm -v $(CURDIR)\build\node2\evmosd:/evmos\Z localnet/node "./evmosd tendermint unsafe-reset-all --home=/evmos"
+	@docker run --rm -v $(CURDIR)\build\node3\evmosd:/evmos\Z localnet/node "./evmosd tendermint unsafe-reset-all --home=/evmos"
+else
+	@docker run --rm -v $(CURDIR)/build/node0/evmosd:/evmos:Z localnet/node "./evmosd tendermint unsafe-reset-all --home=/evmos"
+	@docker run --rm -v $(CURDIR)/build/node1/evmosd:/evmos:Z localnet/node "./evmosd tendermint unsafe-reset-all --home=/evmos"
+	@docker run --rm -v $(CURDIR)/build/node2/evmosd:/evmos:Z localnet/node "./evmosd tendermint unsafe-reset-all --home=/evmos"
+	@docker run --rm -v $(CURDIR)/build/node3/evmosd:/evmos:Z localnet/node "./evmosd tendermint unsafe-reset-all --home=/evmos"
+endif
+
+# Clean testnet
+localnet-show-logstream:
+	docker-compose logs --tail=1000 -f
+
+.PHONY: localnet-build localnet-start localnet-stop
---
+++ localnet/evmos/start.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+KEY="dev0"
+CHAINID="evmos_9000-1"
+MONIKER="mymoniker"
+DATA_DIR=$(mktemp -d -t evmos-datadir.XXXXX)
+MNEMONIC="stumble tilt business detect father ticket major inner awake jeans name vibrant tribe pause crunch sad wine muscle hidden pumpkin inject segment rocket silver"
+
+GENESIS=$DATA_DIR/config/genesis.json
+TEMP_GENESIS=$DATA_DIR/config/tmp_genesis.json
+CONFIG=$DATA_DIR/config/config.toml
+
+echo "create and add new keys"
+echo $MNEMONIC | ./evmosd keys add $KEY --home $DATA_DIR --no-backup --chain-id $CHAINID --keyring-backend test --recover
+echo "init Evmos with moniker=$MONIKER and chain-id=$CHAINID"
+./evmosd init $MONIKER --chain-id $CHAINID --home $DATA_DIR
+echo "prepare genesis: Allocate genesis accounts"
+./evmosd add-genesis-account \
+"$(./evmosd keys show $KEY -a --home $DATA_DIR --keyring-backend test)" 100000000000000000000000000000000aevmos,1000000000000000000stake \
+--home $DATA_DIR --keyring-backend test
+
+# Set gas limit in genesis
+cat $GENESIS | jq '.consensus_params["block"]["max_gas"]="10000000"' > $TEMP_GENESIS && mv $TEMP_GENESIS $GENESIS
+
+echo "prepare genesis: Sign genesis transaction"
+./evmosd gentx $KEY 1000000000000000000stake --keyring-backend test --home $DATA_DIR --keyring-backend test --chain-id $CHAINID
+echo "prepare genesis: Collect genesis tx"
+./evmosd collect-gentxs --home $DATA_DIR
+sed -i 's/aphoton/aevmos/g' $GENESIS
+
+echo "prepare genesis: Run validate-genesis to ensure everything worked and that the genesis file is setup correctly"
+./evmosd validate-genesis --home $DATA_DIR
+
+sed -i 's/prometheus = false/prometheus = true/g' $CONFIG
+sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "0.0.0.0:6060"/g' $CONFIG
+# Change to 1s to have the same default configuration as v9
+sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' "$CONFIG"
+
+
+echo "running evmos with extra flags $EXTRA_FLAGS"
+
+echo "starting evmos node $i in background ..."
+./evmosd start --pruning=nothing --rpc.unsafe \
+--keyring-backend test --home $DATA_DIR \
+>$DATA_DIR/node.log $EXTRA_FLAGS
+
+# curl localhost:8545 -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBalance","params":["0x1cF80B60F4F58221AaFFDBb2e513C0Ef1F809494", "latest"],"id":1,"jsonrpc":"2.0"}'
