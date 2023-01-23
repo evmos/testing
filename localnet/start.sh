5b630fe821ce5d42fb0d0bcb30c853efb255bab7
tom <tomasguerraalda@hotmail.com>
2023-01-23 16:08:09 -0300

    chore(tests): add env variable for pruning settings

--- localnet/start.sh
+++ localnet/start.sh
@@ -15,8 +15,20 @@ sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' "$CONFIG"
 sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "0.0.0.0:6060"/g' $CONFIG
 sed -i 's/127.0.0.1/0.0.0.0/g' "$APP_CONFIG"
 
+# pruning settings
+# if pruning is defined
+if [[ -z "${pruning}" ]]; then 
+    pruning="--pruning=nothing"
+else
+    pruning=""
+    sed -i 's/pruning = "default"/pruning = "custom"/g' $APP_CONFIG
+    sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "5"/g' $APP_CONFIG
+    sed -i 's/pruning-interval = "0"/pruning-interval = "10"/g' $APP_CONFIG
+fi
+
 echo "running $CHAIN with extra flags $EXTRA_FLAGS"
-echo "starting $CHAIN node $i in background ..."
-./$CHAIND start --pruning=nothing --rpc.unsafe \
+echo "starting $CHAIN node in background ..."
+echo "./"$CHAIND" start "$pruning" --rpc.unsafe --keyring-backend test --home "$DATA_DIR" >"$DATA_DIR"/node.log "$EXTRA_FLAGS""
+./$CHAIND start $pruning --rpc.unsafe \
 --keyring-backend test --home $DATA_DIR \
 >$DATA_DIR/node.log $EXTRA_FLAGS
