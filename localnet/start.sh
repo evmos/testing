ea41eded4980f9c168cef42f80023be476a66aa6
tom <tomasguerraalda@hotmail.com>
2023-01-23 12:39:33 -0300

    chore(fix): update start script to make sure localhost is 0.0.0.0

--- localnet/start.sh
+++ localnet/start.sh
@@ -6,11 +6,14 @@ if [[ -z "${CHAIN}" ]]; then CHAIN="evmos"; fi
 CHAIND="$CHAIN"d
 DATA_DIR=/$CHAIN
 CONFIG=$DATA_DIR/config/config.toml
+APP_CONFIG=$DATA_DIR/config/app.toml
 
 sed -i 's/prometheus = false/prometheus = true/g' $CONFIG
-sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "0.0.0.0:6060"/g' $CONFIG
 # Change to 1s to have the same default configuration as v9
 sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' "$CONFIG"
+# make sure the localhost IP is 0.0.0.0
+sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "0.0.0.0:6060"/g' $CONFIG
+sed -i 's/127.0.0.1/0.0.0.0/g' "$APP_CONFIG"
 
 echo "running $CHAIN with extra flags $EXTRA_FLAGS"
 echo "starting $CHAIN node $i in background ..."
