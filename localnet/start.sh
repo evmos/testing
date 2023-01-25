b2bb80d318cb0891e2dcc23e4c77f8f1bf8b1ffe
MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
2023-01-25 10:32:22 +0100

    Apply suggestions from code review

--- localnet/start.sh
+++ localnet/start.sh
@@ -12,7 +12,7 @@ sed -i 's/prometheus = false/prometheus = true/g' $CONFIG
 # Change to 1s to have the same default configuration as v9
 sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' "$CONFIG"
 # make sure the localhost IP is 0.0.0.0
-sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "0.0.0.0:6060"/g' $CONFIG
+sed -i 's/pprof_laddr = "localhost:6060"/pprof_laddr = "0.0.0.0:6060"/g' "$CONFIG"
 sed -i 's/127.0.0.1/0.0.0.0/g' "$APP_CONFIG"
 
 # pruning settings
@@ -21,9 +21,9 @@ if [[ -z "${pruning}" ]]; then
     pruning="--pruning=nothing"
 else
     pruning=""
-    sed -i 's/pruning = "default"/pruning = "custom"/g' $APP_CONFIG
-    sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "5"/g' $APP_CONFIG
-    sed -i 's/pruning-interval = "0"/pruning-interval = "10"/g' $APP_CONFIG
+    sed -i 's/pruning = "default"/pruning = "custom"/g' "$APP_CONFIG"
+    sed -i 's/pruning-keep-recent = "0"/pruning-keep-recent = "5"/g' "$APP_CONFIG"
+    sed -i 's/pruning-interval = "0"/pruning-interval = "10"/g' "$APP_CONFIG"
 fi
 
 echo "running $CHAIN with extra flags $EXTRA_FLAGS"
